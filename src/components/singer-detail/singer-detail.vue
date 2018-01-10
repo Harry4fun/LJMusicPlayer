ce1445dbbaac78234d07b390989f2ed5bf036deb
Harry4fun <jiaojingkuan123@163.com>
2018-01-10 22:47:10 +0800

    封装song-list插件
    Signed-off-by: Harry4fun <jiaojingkuan123@163.com>

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,6 @@
 <template>
 <transition class="slide">
-  <div class="singer-detail">gfdgdfgdfg
-  </div>
+  <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
 </transition>
 
 </template>
@@ -10,6 +9,7 @@ import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import {createSong} from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
 export default {
   data () {
@@ -18,6 +18,13 @@ export default {
     }
   },
   computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      console.log('当前歌手的头像地址：', this.singer.avatar)
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
@@ -57,20 +64,23 @@ export default {
       })
       return ret
     }
+  },
+  components: {
+    MusicList
   }
 }
 </script>
 <style lang="stylus" scoped rel="stylesheet/stylus"> 
 @import "~common/stylus/variable"
 
-.singer-detail
-  position: fixed
-  z-index: 100
-  top: 0
-  left: 0
-  right: 0
-  bottom: 0
-  background-color: $color-background
+// .singer-detail
+//   position: fixed
+//   z-index: 100
+//   top: 0
+//   left: 0
+//   right: 0
+//   bottom: 0
+//   background-color: $color-background
 
 .slide-enter-active, .slide-leave-active
   transition: all 0.3s
